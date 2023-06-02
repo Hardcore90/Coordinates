464383a75b0dea402b08e1e97dbd7c4439526df0
Dmitriy <maradonna05@yandex.ru>
2023-06-02 12:54:07 +0300

    Добавьте файлы проекта.

---
+++ Coordinates/Coordinates.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <math.h>
+using namespace std;
+int main()
+{
+/*Задание №1
+
+Реализуйте программу, которая считывает из файла наборы координат и выводит только те координаты, которые расположены в квадрате :
+
+Широта: от 50 до 80
+
+Долгота : от 20 до 45 */
+
+	ifstream ifile("coordinates.txt", ios::in);
+	if (ifile.is_open()) 
+	{	
+		
+		
+		while (!ifile.eof())
+		{		
+			string line;
+			double lat, lon;
+			getline(ifile, line);
+			//cout << line << endl;
+			stringstream ss(line);
+			ss.ignore(1);
+			ss >> lat;
+			ss.ignore(2);
+			ss >> lon;
+			
+			if (lat > 50 && lon > 20)
+			{			
+				if (lat < 80 && lon < 45)
+				{
+					cout << lat << ", " << lon << endl;
+					cout << endl;
+				}							
+			}			
+		}		
+	}
+	
+
+	return 0;
+}
+
